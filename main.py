bf01c89a0487c8cfe04d9369cff8dfbbb397fa75
szjung-test <szdang@naver.com>
2022-10-06 17:03:31 +0900

    Body

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from doctest import Example
 from typing import List, Set, Union, Dict
 from unittest import result
 #from unittest import result
 
 from fastapi import Body, FastAPI, Path
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, Field
 
 app = FastAPI()
 
@@ -17,20 +18,67 @@ class Item(BaseModel):
     description: Union[str, None] = None
     price: float
     tax: Union[float, None] = None
-    tags: Set[str] = set()
-    images: Union[Image, None] = None
+    
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     tax: Union[float, None] = None
+#     tags: Set[str] = set()
+#     images: Union[Image, None] = None
 
-class Offer(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    items: List[Item]
+# class Item(BaseModel):
+#     name: str = Field(example="Foo")
+#     description: Union[str, None] = Field(default=None, example="A very nice Item")
+#     price: float = Field(example=35.4)
+#     tax: Union[float, None] = Field(default=None, example=3.2)
+
+# class Offer(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     items: List[Item]
 
 # class User(BaseModel):
 #     username: str
 #     full_name: Union[str, None] = None
 
-
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int,
+    item: Item = Body(
+        examples={
+            "normal": {
+                "summary": "A normal example",
+                "description": "A **normal** item works correctly.",
+                "value": {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+            },
+            "converted": {
+                "summary": "An example with converted data",
+                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                "value": {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+            },
+            "invalid": {
+                "summary": "Invalid data is rejected with an error",
+                "value": {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            },
+        },
+    ),
+):
+    results = {"item_id": item_id, "item": item}
+    return results
 
 # @app.put("/items/{item_id}")
 # async def update_item(
@@ -79,6 +127,34 @@ class Offer(BaseModel):
 # async def create_multiple_images(images: List[Image]):
 #     return images
 
-@app.post("/index-weights/")
-async def create_index_weights(weights: Dict[int, float]):
-    return weights
+# @app.post("/index-weights/")
+# async def create_index_weights(weights: Dict[int, float]):
+#     return weights
+
+# pydantic "schema_extra"
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     tax: Union[float, None] = None
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     item_id: int,
+#     item: Item = Body(
+#         example={
+#             "name": "Foo",
+#             "description": "A very nice Item",
+#             "price": 35.4,
+#             "tax": 3.2,
+#         },
+#     ),
+# ):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: int):
+#     results = {"item_id": item_id, "item": item}
+#     return results
